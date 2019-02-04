032b02ca4f23e3277036c04324ce647617a09d77
Anthony Beacock <tonybeacock@hotmail.co.uk>
2019-02-04 21:26:57 +0000

    Created Scene Manager
    Created basic Scene Manager
    Created basic Scene Class

---
+++ KodeboldsEngineMK2/Scene.cpp
@@ -0,0 +1,24 @@
+#include "Scene.h"
+
+
+
+void Scene::Render()
+{
+}
+
+void Scene::Update()
+{
+}
+
+void Scene::OnLoad()
+{
+}
+
+Scene::Scene()
+{
+}
+
+
+Scene::~Scene()
+{
+}
---
+++ KodeboldsEngineMK2/Scene.h
@@ -0,0 +1,12 @@
+#pragma once
+class Scene
+{
+private:
+public:
+	Scene();
+	~Scene();
+	void Render();
+	void Update();
+	void OnLoad();
+};
+
