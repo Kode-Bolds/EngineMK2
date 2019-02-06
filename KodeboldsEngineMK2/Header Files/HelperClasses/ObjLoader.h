cb6c41fa0eade2d6cddd3f391b16c5d199806297
JakeScrivener <Jake.Scrivener55@gmail.com>
2019-02-06 14:20:20 +0000

    fixed some issues in resource manager and added obj loader

---
+++ KodeboldsEngineMK2/Header Files/HelperClasses/ObjLoader.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <d3d11.h>
+#include <vector>
+
+class ObjLoader
+{
+	//std::vector<unsigned> mIndices;
+	//std::vector<std::tuple<float,float,float>> mVertices;
+	//std::vector<unsigned> mTexIndices;
+	//std::vector<float, float> mTexCoords;
+	//std::vector<unsigned> mNormalIndices;
+	//std::vector<float, float> mNormals;
+
+public:
+	ObjLoader();
+	~ObjLoader();
+
+	//std::pair<std::vector<unsigned>, std::vector<>> LoadObject(std::string& pFilename);
+};
+
