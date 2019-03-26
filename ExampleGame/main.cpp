91431f7d4dbe36053b436a6bed5d07ddf4df00d1
Dean <deanb1996@hotmail.co.uk>
2019-03-26 19:38:56 +0000

    Graphics debugging

--- ExampleGame/main.cpp
+++ ExampleGame/main.cpp
@@ -64,13 +64,13 @@ int WINAPI wWinMain(_In_ const HINSTANCE pHInstance, _In_opt_ const HINSTANCE pH
 
 	//Testing transform component
 	Transform transform;
-	transform.mTranslation = Vector4(0.0f, 0.0f, 0.0f, 1.0f);
+	transform.mTranslation = Vector4(0.0f, 0.0f, -1.0f, 1.0f);
 	ecsManager->AddTransformComp(transform, "Cam");
 	Camera camera{Vector4(0.0f, 0.0f, 5.0f, 1.0f), Vector4(0.0f, 1.0f, 0.0f, 1.0f), 60, 1, 500};
 	ecsManager->AddCameraComp(camera, "Cam");
 
 	transform.mTranslation = Vector4(0.0f, 0.0f, 5.0f, 1.0f);
-	transform.mTransform = TranslationMatrix(transform.mTranslation);
+	transform.mTransform = TranslationMatrix(transform.mTranslation) * ScaleMatrix(Vector4(1.0f, 1.0f, 1.0f, 1.0f));
 	ecsManager->AddTransformComp(transform, "Cube");
 	Geometry geometry{L"cube.obj"};
 	ecsManager->AddGeometryComp(geometry, "Cube");
